ff48d6ea6a1b5e74241d856c7e590c9f30dfa865
Sd37289 <sd37289@gmail.com>
2023-10-19 13:51:27 +0300

    Flutter Projesi
    Alt yapı çalışmalarını paket haline getirme için yapılan çalışma.

---
+++ uyum_package/lib/src/uyum_widgets.dart
@@ -0,0 +1,63 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+
+
+
+class UyumCheckBox extends StatefulWidget {
+  UyumCheckBox(
+      {Key? key,
+        this.ActiveColor,
+        this.CheckColor,
+        this.BorderColor,
+        this.BorderWidth})
+      : super(key: key);
+
+  final Color? ActiveColor;
+  final Color? CheckColor;
+  final Color? BorderColor;
+  final double? BorderWidth;
+
+  @override
+  State<UyumCheckBox> createState() => _UyumCheckBoxState();
+}
+
+class _UyumCheckBoxState extends State<UyumCheckBox> {
+  bool? Value = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: Value,
+      activeColor: widget.ActiveColor ?? UyumColor().UyumRed,
+      checkColor: widget.CheckColor ?? UyumColor().UyumWhite,
+      side: BorderSide(
+        color: widget.BorderColor ?? UyumColor().UyumBlack,
+        width: widget.BorderWidth ?? 2,
+      ),
+      onChanged: (bool? value) {
+        setState(() {
+          Value = value!;
+        });
+      },
+    );
+  }
+}
+
+class UyumTheme {
+  ThemeData themeData = ThemeData(
+    appBarTheme: AppBarTheme(
+      backgroundColor: UyumColor().UyumRed,
+    ),
+    bottomAppBarTheme: BottomAppBarTheme(
+      color: UyumColor().UyumRed,
+    ),
+  );
+}
+
+class UyumColor { 
+  final Color UyumRed = const Color.fromRGBO(186, 43, 43, 1);
+  final Color UyumBlack = const Color.fromRGBO(74, 72, 72, 1);
+  final Color UyumWhite = Colors.white;
+  final Color UyumBlue = Colors.blue;
+}
---
+++ uyum_package/lib/uyum_package.dart
@@ -0,0 +1,92 @@
+library uyum_package;
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:uyum_package/src/uyum_widgets.dart';
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
+
+class UyumBottomAppBar extends StatelessWidget {
+
+  const UyumBottomAppBar({
+    Key? key,
+    this.bottoAppBarLeftIcon,
+    this.bottoAppBarRightIcon,
+    this.bottonAppBarText,
+  }) : super(key: key);
+
+  final IconData? bottoAppBarLeftIcon;
+  final IconData? bottoAppBarRightIcon;
+  final String? bottonAppBarText;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget ResultWidget = Container();
+
+    // TODO SE: Burada butonların ve textin boutları  >> BottomAppBar << ın boyutuan göre ayarlanacak.
+    final double LeftIconSize =35 ;
+    final double RightIconSize = 50;
+    final double TextSize = 28;
+
+    ResultWidget = BottomAppBar(
+      child: InkWell(
+        // TODO SE : >> BottomAppBar << ın tetikleyeceği fonksiyon dinamik olark ayarlanacak.
+        onTap: () {},
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            bottoAppBarLeftIcon != null
+                ? Icon(
+              bottoAppBarLeftIcon,
+              color: UyumColor().UyumWhite,
+              size: LeftIconSize,
+            )
+                : Container(),
+            bottonAppBarText != null
+                ? Text(
+              bottonAppBarText!,
+              style: TextStyle(color: UyumColor().UyumWhite, fontSize: TextSize, fontWeight: FontWeight.w900),
+            )
+                : Container(),
+            bottoAppBarRightIcon != null
+                ? Icon(
+              bottoAppBarRightIcon,
+              color: UyumColor().UyumWhite,
+              size: RightIconSize,
+            )
+                : Container(),
+          ],
+        ),
+      ),
+    );
+
+    return ResultWidget;
+  }
+}
+
+
+
+class UyumTheme {
+  ThemeData themeData = ThemeData(
+    appBarTheme: AppBarTheme(
+      backgroundColor: UyumColor().UyumRed,
+    ),
+    bottomAppBarTheme: BottomAppBarTheme(
+      color: UyumColor().UyumRed,
+    ),
+  );
+}
+
+class UyumColor { 
+  final Color UyumRed = const Color.fromRGBO(186, 43, 43, 1);
+  final Color UyumBlack = const Color.fromRGBO(74, 72, 72, 1);
+  final Color UyumWhite = Colors.white;
+  final Color UyumBlue = Colors.blue;
+}
+
+
+
+
